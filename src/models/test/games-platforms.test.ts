7d72b3c3aa474c1235764ab8303ccf2e523a8d87
Sarah <sarah.hammadi808@gmail.com>
2020-10-22 22:26:41 +0200

    games-platforms test update

---
+++ src/models/test/games-platforms.test.ts
@@ -0,0 +1,44 @@
+import { openBrowser, closeBrowser, goto, text, click, waitFor} from "taiko";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+describe("Explore Games and Platforms ", () => {
+  jest.setTimeout(20000);
+
+  beforeAll(async () => {
+    await openBrowser({
+      args: [
+        "--window-size=1280,800",
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-first-run",
+        "--no-sandbox",
+        "--no-zygote",
+      ],
+      headless: false,
+    });
+  });
+
+  afterAll(async () => {
+    await closeBrowser();
+  });
+  test("Games appear when we click on Explore games", async () => {
+    expect.assertions(1)
+    const website = process.env.URL || "";
+    await goto(website);
+    await click("Explore Video Games");
+    await waitFor("Games");
+    expect(await text("Games").exists()).toBeTruthy();
+});
+test("Platforms appear when we click on Explore Platforms", async () => {
+    expect.assertions(1)
+    const website = process.env.URL || "";
+    await goto(website);
+    await click("Explore Platforms");
+    await waitFor("Platforms");
+    expect(await text("Platforms").exists()).toBeTruthy();
+});
+
+});
--- src/models/test/login.test.ts
+++ src/models/test/login.test.ts
@@ -1,4 +1,7 @@
 import { openBrowser, closeBrowser, goto, text, click, waitFor, write, into, textBox } from "taiko";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 
 describe("We can open our website and Login", () => {
   jest.setTimeout(20000);
@@ -22,18 +25,20 @@ describe("We can open our website and Login", () => {
     await closeBrowser();
   });
 
-  test.only("We can Login", async () => {
+  test("We can Login", async () => {
     expect.assertions(1);
-
+    const password = process.env.PASSWORD|| "";
+    const mail = process.env.EMAIL || "";
     const website = process.env.URL || "";
     await goto(website);
     await click("Login");
     await click("Do you want to log ?");
-    await write('sarahtina38@gmail.com', into(textBox("Email")));
+    await write(mail, into(textBox("Email")));
     await click("NEXT");
-    await write('codepassecret', into(textBox("Password")));
+    await write(password, into(textBox("Password")));
     await click("NEXT");
     await waitFor("Logout");
     expect(await text("Logout").exists()).toBeTruthy();
   });
-});
+  });
+  
